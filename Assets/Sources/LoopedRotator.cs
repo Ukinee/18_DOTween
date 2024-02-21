469fe7bd92fac6e424f77bd2edad4badf45c948b
Сергей Куделин <xperia.zed@mail.ru>
2024-02-21 18:34:08 +0300

    Initial commit

---
+++ Assets/Sources/LoopedRotator.cs
@@ -0,0 +1,19 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class LoopedRotator : MonoBehaviour
+{
+    [SerializeField] private float _duration = 3f;
+    [SerializeField] private int _loopCount = -1;
+
+    void Start()
+    {
+        transform
+            .DOLocalRotateQuaternion
+            (
+                Quaternion.LookRotation(Vector3.back),
+                _duration
+            )
+            .SetLoops(_loopCount, LoopType.Restart);
+    }
+}
